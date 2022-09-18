322df18efdf018e5e87c8dc1d3ff88b57331b2fc
BrKenji <ninihiratafei@gmail.com>
2022-09-18 04:47:03 -0300

    Added DELETE method
    Removed EXPOSE from Dockerfile

--- src/main/java/com/api/forum/controller/TopicosController.java
+++ src/main/java/com/api/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,7 @@ public class TopicosController {
 	// @Valid bean, warns springs that the form, pulling the data from the request, to run the validation beans
 	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> createTopic(@RequestBody @Valid TopicoForm topicoForm,
 			UriComponentsBuilder uriBuilder) {
 		
@@ -73,6 +75,15 @@ public class TopicosController {
 		
 		return ResponseEntity.ok(new TopicoDto(topico));
 		
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> deleteTopico(@PathVariable Long id){
+		
+		topicoRepository.deleteById(id);
+		
+		return ResponseEntity.ok().build();
 	}
 	
 	@GetMapping("/count")

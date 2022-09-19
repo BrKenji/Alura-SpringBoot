7de829e5255dbfc2cf73a91b253ece11f5802f55
BrKenji <ninihiratafei@gmail.com>
2022-09-19 15:59:09 -0300

    Spring Boot API Rest: Segurança da API, Cache e Monitoramento
    Uso de paginação no método GET que retorna todos os tópicos

--- src/main/java/com/api/forum/controller/TopicosController.java
+++ src/main/java/com/api/forum/controller/TopicosController.java
@@ -8,6 +8,9 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -38,13 +42,18 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public List<TopicoDto> getTopicsList(String nomeCurso){
+	public Page<TopicoDto> getAllTopics(@RequestParam(required = false) String nomeCurso,
+			@RequestParam(required = true) int page,
+			@RequestParam(required = true) int qtd) {
+		
+		// Pageable é uma interface do spring data usado para paginação
+		Pageable paginacao = PageRequest.of(page, qtd);
 		
 		if(nomeCurso == null) {
-			List<Topico> topicos = topicoRepository.findAll();
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.topicModelToDto(topicos);
 		} else {
-			List<Topico> topicos = topicoRepository.getByCourseName(nomeCurso);
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
 			return TopicoDto.topicModelToDto(topicos);	
 		}
 		
--- src/main/java/com/api/forum/repository/TopicoRepository.java
+++ src/main/java/com/api/forum/repository/TopicoRepository.java
@@ -2,6 +2,8 @@ package com.api.forum.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +15,6 @@ public interface TopicoRepository extends JpaRepository<Topico, Long>{
 	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
 	List<Topico> getByCourseName(@Param("nomeCurso") String nomeCurso);
 	
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 	
 }
